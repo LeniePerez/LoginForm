cdff7c1fbfbcbc92a3fde97706d71e0d64218965
LeniePerez <perezlenieanne104018@gmail.com>
2021-01-01 13:53:52 +0800

    Initial commit

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,45 @@
+﻿using System.Data.SqlClient;
+using LoginForm.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoginForm.Controllers
+{
+    public class AccountController : Controller
+    {
+        SqlConnection con = new SqlConnection();
+        SqlCommand com = new SqlCommand();
+        SqlDataReader dr;
+        //getaccount
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        void connectionString()
+        {
+            con.ConnectionString = @"data source =USER-PC\SQLEXPRESS; database=WPF; integrated security = SSPI;";
+        }
+        public ActionResult Verify(Account acc)
+        {
+            connectionString();
+            con.Open();
+            com.Connection = con;
+            com.CommandText = "select * from login where Identification_No='"+acc.Identification_No+"' and Password='"+acc.Password+"'";
+            dr = com.ExecuteReader();
+            if (dr.Read())
+            {
+                con.Close();
+                return View();
+            }
+            else
+            {
+                con.Close();
+                return View();
+            }
+
+            
+            
+        }
+    }
+}
---
+++ Models/Account.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoginForm.Models
+{
+    public class Account
+    {
+        public string Identification_No { get; set; }
+        public string Password { get; set; }
+
+
+    }
+}
